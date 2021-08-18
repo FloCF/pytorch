efb6d91da2d9ffef7255826972cd2ca97c9da738
Brian Hirsh <hirsheybar@fb.com>
2021-08-18 07:55:51 -0700

    plumb c10::nullopt through python binding layer when dtype and device aren't specified
    ghstack-source-id: fecbba6f22928abf21069eddeb89dc249aaae7ad
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63364

--- tools/codegen/api/python.py
+++ tools/codegen/api/python.py
@@ -695,7 +695,7 @@ def signature(f: NativeFunction, *, method: bool = False, pyi: bool = False) -> 
     if is_factory_function or is_like_or_new_function:
         tensor_options_args.append(PythonArgument(
             name='dtype',
-            type=BaseType(BaseTy.ScalarType),
+            type=OptionalType(BaseType(BaseTy.ScalarType)),
             default='None' if pyi else _dtype_default_type_hack(name),
             default_init='self.scalar_type()' if is_like_or_new_function else None,
         ))
@@ -707,7 +707,13 @@ def signature(f: NativeFunction, *, method: bool = False, pyi: bool = False) -> 
         ))
         tensor_options_args.append(PythonArgument(
             name='device',
-            type=BaseType(BaseTy.Device),
+            # Note: This should really be an Optional type, but changing that behavior would be BC-breaking.
+            # Currently, when no device argument is specified in a factory function, the python binding layer
+            # defaults the device to the device set by `torch.set_default_tensor_type`.
+            # If instead we defaulted to c10::nullopt, then factory functions would eventually use the default device
+            # of TensorOptions, which is kCPU - effectively ignoring `torch.set_default_tensor_type`.
+            # See https://github.com/pytorch/pytorch/pull/63364#issuecomment-900590178.
+            type=OptionalType(BaseType(BaseTy.Device)) if name == 'empty_quantized' else BaseType(BaseTy.Device),
             default='None',
             default_init='self.device()' if is_like_or_new_function else None,
         ))
@@ -997,7 +1003,7 @@ def cpp_dispatch_exprs(f: NativeFunction, *,
 # For certain cases it is intentionally more restrictive than necessary,
 # e.g.: it doesn't accepts doublelist with definite size.
 def arg_parser_unpack_method(t: Type, has_default: bool) -> str:
-    if has_default and str(t) not in ('ScalarType', 'Device', 'Layout?'):
+    if has_default and str(t) not in ('ScalarType?', 'Device', 'Layout?'):
         raise RuntimeError(f'type \'{t}\' does not supported unpacking with default')
 
     if isinstance(t, BaseType):
@@ -1023,7 +1029,7 @@ def arg_parser_unpack_method(t: Type, has_default: bool) -> str:
             return 'optionalTensor'
 
         elif isinstance(t.elem, BaseType):
-            if t.elem.name in [BaseTy.ScalarType, BaseTy.Scalar,
+            if t.elem.name in [BaseTy.Scalar,
                                BaseTy.int, BaseTy.bool,
                                BaseTy.float, BaseTy.str]:
                 # Regular cases: append 'Optional' to elem's unpacking method
@@ -1036,6 +1042,8 @@ def arg_parser_unpack_method(t: Type, has_default: bool) -> str:
                 return 'layoutWithDefault' if has_default else 'layoutOptional'
             elif t.elem.name == BaseTy.Device:
                 return 'deviceWithDefault' if has_default else 'deviceOptional'
+            elif t.elem.name == BaseTy.ScalarType:
+                return 'scalartypeWithDefault' if has_default else 'scalartypeOptional'
 
         elif isinstance(t.elem, ListType):
             if str(t.elem.elem) == 'int':
@@ -1093,7 +1101,7 @@ def arg_parser_output_exprs(
 
 # argument name to type for scattered tensor options fields
 TENSOR_OPTIONS_FIELDS = {
-    'dtype': 'ScalarType',
+    'dtype': 'ScalarType?',
     'device': 'Device',
     'layout': 'Layout?',
     'pin_memory': 'bool',
@@ -1191,7 +1199,7 @@ torch::utils::maybe_initialize_cuda(options);
 
             inits.append(f"""\
 check_out_type_matches({arg_parser_outputs['out'].expr}, {arg_parser_outputs['dtype'].expr},
-                       {arg_parser_outputs['dtype'].is_none_expr}, {arg_parser_outputs['layout'].expr},
+                       {arg_parser_outputs['layout'].expr},
                        {arg_parser_outputs['device'].expr}, {arg_parser_outputs['device'].is_none_expr});
 """)
         # we'll set requires_grad on outgoing tensor
